8c3d1ea982281aed877352f280660684e9eaca66
Thibault-GX <thibault.grimoux@gmail.com>
2019-10-25 15:14:55 +0200

    Push initial.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let verification = function()
+{
+  let age = document.getElementById("age").value;
+  if (age >= 18) {
+    alert("Bienvenue mon chou !")
+  } else {
+    alert("Allez, ça dégage !")
+  }
+}
